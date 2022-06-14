059edea00b3c4198820f3309b0ae472a412f3f3f
thanhdat77 <tdat772001@google.com>
2022-06-14 17:39:09 +0700

    add requirement and utils

---
+++ asr_toolkit/utils.py
@@ -0,0 +1,10 @@
+import pydub
+def mp3ToWav(path):
+    try:
+        sound = pydub.AudioSegment.from_mp3(path)
+        path = str(path)
+        new_path =path[:-3] + 'wav'
+        sound.export(new_path, format="wav")
+        return new_path
+    except:
+        print(path + "khong the conver to wav")
