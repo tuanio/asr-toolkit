1e97ea7fea641563b0f7e0c5ee91fba3c190bc4c
Nguyen Van Anh Tuan <nvatuan3@gmail.com>
2022-06-26 00:14:03 +0700

    edit positional encoding

--- asr_toolkit/embedding.py
+++ asr_toolkit/embedding.py
@@ -19,7 +19,7 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(max_len, d_model, requires_grad=False)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(
-            torch.arange(0, d_model, 2).float() * -(math.log(max_len) / d_model)
+            torch.arange(0, d_model, 2).float() * -(math.log(10000) / d_model)
         )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
